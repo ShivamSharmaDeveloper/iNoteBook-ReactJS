570db62d2d65ef134632e8bede1ae9fa710c57c5
Shivam Sharma <shivsharma7821@gmail.com>
2023-05-02 18:53:44 +0530

    created add user in database

---
+++ backend/routes/auth.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const User = require("../models/User");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
+
+// Create a user using: POST "/api/auth". Doesn't require Auth
+router.post(
+  "/",
+  [
+    body("name", "Enter a valid Name").isLength({ min: 3 }),
+    body("email", "Enter a valid Email").isEmail(),
+    body("password", "Password must be 5 characters").isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        res.json({ error: "Please enter a unique email!" });
+      });
+  }
+);
+
+module.exports = router;
