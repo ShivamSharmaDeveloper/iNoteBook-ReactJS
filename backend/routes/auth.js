b8e54f328181e13fd5bd0601a6894b5c62e67597
Shivam Sharma <shivsharma7821@gmail.com>
2023-05-11 13:08:26 +0530

    Added User functionality

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,29 +3,44 @@ const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 
-// Create a user using: POST "/api/auth". Doesn't require Auth
+// Create a user using: POST "/api/auth/createuser". Doesn't require Auth
 router.post(
-  "/",
+  "/createuser",
   [
     body("name", "Enter a valid Name").isLength({ min: 3 }),
     body("email", "Enter a valid Email").isEmail(),
     body("password", "Password must be 5 characters").isLength({ min: 5 }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    // If there are errors, return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log(err);
-        res.json({ error: "Please enter a unique email!" });
+    try {
+      // Check whether the user with this email exists already
+      let user = await User.findOne({ email: req.body.email });
+      // console.log(user);
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists." });
+      }
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
       });
+      // .then((user) => res.json(user))
+      // .catch((err) => {
+      //   console.log(err);
+      //   res.json({ error: "Please enter a unique email!" });
+      // });
+      res.json(user);
+    } catch (error) {
+      console.error(error.message)
+      res.status(500).send("Some Error occured!")
+    }
   }
 );
 
