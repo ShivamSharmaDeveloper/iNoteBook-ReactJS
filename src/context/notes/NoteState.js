023ec726df915a5808bc62118e8e4fc03a98e1c0
Shivam Sharma <shivsharma7821@gmail.com>
2023-05-12 19:05:41 +0530

    ✨ feat(index.js): update console log message to include app name for better identification ✨ feat(App.js): add container class to wrap Routes for better UI layout ✨ feat(About.js): remove unused context and update return message ✨ feat(Home.js): add form and Notes component to Home page ✨ feat(Navbar.js): add active class to current page link and update navbar theme ✨ feat(NoteItem.js): create new component to display individual note item ✨ feat(Notes.js): create new component to display all notes using NoteItem component ✨ feat(NoteState.js): add initial notes state and setNotes function to context provider

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,23 +2,67 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-  const s1 = {
-    name: "Shivam",
-    class: "6b",
-  };
-  const [state, setState] = useState(s1);
-  const update = () => {
-    setTimeout(() => {
-      setState({
-        name: "Sahil",
-        class: "6a",
-      });
-    }, 1000);
-  };
+  const notesInitial = [
+    {
+      _id: "645cdca823d5785bbf61663a",
+      user: "645ca18184c15866ab5550e7",
+      title: "My Tittle",
+      description: "My First test note added.",
+      tag: "test",
+      date: "2023-05-11T12:16:40.139Z",
+      __v: 0,
+    },
+    {
+      _id: "645e364a85c013c8d36b060d",
+      user: "645ca18184c15866ab5550e7",
+      title: "My Second Tittle",
+      description: "My Second test note added.",
+      tag: "test",
+      date: "2023-05-12T12:51:22.982Z",
+      __v: 0,
+    },
+    {
+      _id: "645e364a85c013c8d36b060d",
+      user: "645ca18184c15866ab5550e7",
+      title: "My Second Tittle",
+      description: "My Second test note added.",
+      tag: "test",
+      date: "2023-05-12T12:51:22.982Z",
+      __v: 0,
+    },
+    {
+      _id: "645e364a85c013c8d36b060d",
+      user: "645ca18184c15866ab5550e7",
+      title: "My Second Tittle",
+      description: "My Second test note added.",
+      tag: "test",
+      date: "2023-05-12T12:51:22.982Z",
+      __v: 0,
+    },
+    {
+      _id: "645e364a85c013c8d36b060d",
+      user: "645ca18184c15866ab5550e7",
+      title: "My Second Tittle",
+      description: "My Second test note added.",
+      tag: "test",
+      date: "2023-05-12T12:51:22.982Z",
+      __v: 0,
+    },
+    {
+      _id: "645e364a85c013c8d36b060d",
+      user: "645ca18184c15866ab5550e7",
+      title: "My Second Tittle",
+      description: "My Second test note added.",
+      tag: "test",
+      date: "2023-05-12T12:51:22.982Z",
+      __v: 0,
+    },
+  ];
+
+  const [notes, setNotes] = useState(notesInitial);
+
   return (
-    <NoteContext.Provider value={{state, update}}>
-      {props.children}
-    </NoteContext.Provider>
+    <NoteContext.Provider value={{notes, setNotes}}>{props.children}</NoteContext.Provider>
   );
 };
 
