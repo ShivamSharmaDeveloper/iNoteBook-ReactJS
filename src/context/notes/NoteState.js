0d031e7f8bfd170c94d7dbed11aaa36d3c951094
Shivam Sharma <shivsharma7821@gmail.com>
2023-05-13 17:34:58 +0530

    ✨ feat(index.js): add cors middleware to allow cross-origin requests ✨ feat(index.js): add support for process.env.PORT environment variable to be able to run app on a configurable port ✨ feat(Notes.js): add useEffect hook to fetch notes on component mount 🐛 fix(NoteState.js): update notes state after adding a new note 🐛 fix(NoteState.js): update notes state after editing a note

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,70 +1,43 @@
+import { json } from "react-router-dom";
 import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-  const notesInitial = [
-    {
-      _id: "645cdca823d5785bbf61663a1",
-      user: "645ca18184c15866ab5550e7",
-      title: "My Tittle",
-      description: "My First test note added.",
-      tag: "test",
-      date: "2023-05-11T12:16:40.139Z",
-      __v: 0,
-    },
-    {
-      _id: "645e364a85c013c8d36b060d2",
-      user: "645ca18184c15866ab5550e7",
-      title: "My Second Tittle",
-      description: "My Second test note added.",
-      tag: "test",
-      date: "2023-05-12T12:51:22.982Z",
-      __v: 0,
-    },
-    {
-      _id: "645e364a85c013c8d36b060d3",
-      user: "645ca18184c15866ab5550e7",
-      title: "My Second Tittle",
-      description: "My Second test note added.",
-      tag: "test",
-      date: "2023-05-12T12:51:22.982Z",
-      __v: 0,
-    },
-    {
-      _id: "645e364a85c013c8d36b060d4",
-      user: "645ca18184c15866ab5550e7",
-      title: "My Second Tittle",
-      description: "My Second test note added.",
-      tag: "test",
-      date: "2023-05-12T12:51:22.982Z",
-      __v: 0,
-    },
-    {
-      _id: "645e364a85c013c8d36b060d5",
-      user: "645ca18184c15866ab5550e7",
-      title: "My Second Tittle",
-      description: "My Second test note added.",
-      tag: "test",
-      date: "2023-05-12T12:51:22.982Z",
-      __v: 0,
-    },
-    {
-      _id: "645e364a85c013c8d36b060d6",
-      user: "645ca18184c15866ab5550e7",
-      title: "My Second Tittle",
-      description: "My Second test note added.",
-      tag: "test",
-      date: "2023-05-12T12:51:22.982Z",
-      __v: 0,
-    },
-  ];
+  const host = "http://localhost:5000";
+  const notesInitial = [];
 
   const [notes, setNotes] = useState(notesInitial);
 
+  // Get all Notes
+  const getNotes = async () => {
+    // API Call
+    const response = await fetch(`${host}/api/notes/fetchallnotes`, {
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ1Y2ExODE4NGMxNTg2NmFiNTU1MGU3In0sImlhdCI6MTY4Mzc5NDg4MX0.GBbZvxT9FOY-6Q6PsU6SC8pmeouGqk1NMLup_mkoqR0",
+      },
+    });
+    const json = await response.json()
+    console.log(json)
+    setNotes(json)
+  };
   // Add a Note
-  const addNote = (title, description, tag) => {
+  const addNote = async (title, description, tag) => {
     // TODO: API Call
-    console.log("Adding a New Note!")
+    // API Call
+    const response = await fetch(`${host}/api/notes/addnote`, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ1Y2ExODE4NGMxNTg2NmFiNTU1MGU3In0sImlhdCI6MTY4Mzc5NDg4MX0.GBbZvxT9FOY-6Q6PsU6SC8pmeouGqk1NMLup_mkoqR0",
+      },
+      body: JSON.stringify({title, description, tag}), // body data type must match "Content-Type" header
+    });
+
+    console.log("Adding a New Note!");
     const note = {
       _id: "645e364a85c013c8d36b060d7",
       user: "645ca18184c15866ab5550e7",
@@ -87,9 +60,33 @@ const NoteState = (props) => {
     setNotes(newNotes);
   };
   // Edit a Note
-  const editNote = (id, title, description, tag) => {};
+  const editNote = async (id, title, description, tag) => {
+    // API Call
+    const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ1Y2ExODE4NGMxNTg2NmFiNTU1MGU3In0sImlhdCI6MTY4Mzc5NDg4MX0.GBbZvxT9FOY-6Q6PsU6SC8pmeouGqk1NMLup_mkoqR0",
+      },
+      body: JSON.stringify({ title, description, tag }), // body data type must match "Content-Type" header
+    });
+    const json = response.json(); // parses JSON response into native JavaScript objects
+
+    // Logic to exit in client
+    for (let index = 0; index < notes.length; index++) {
+      const element = notes[index];
+      if (element._id === id) {
+        element.title = title;
+        element.description = description;
+        element.tag = tag;
+      }
+    }
+  };
   return (
-    <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>
+    <NoteContext.Provider
+      value={{ notes, addNote, deleteNote, editNote, getNotes }}
+    >
       {props.children}
     </NoteContext.Provider>
   );
