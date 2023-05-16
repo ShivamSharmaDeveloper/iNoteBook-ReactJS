3c77bc5de64b4e57d6160db7e58b8462ff47bbf1
Shivam Sharma <shivsharma7821@gmail.com>
2023-05-16 18:14:26 +0530

    🔒 chore(.env.example): add example .env file with JWT_KEY variable 🔒 chore(.gitignore): add .env file to ignore list 🐛 fix(fetchuser.js): use JWT_KEY variable from .env file instead of hardcoded value 🐛 fix(auth.js): use JWT_KEY variable from .env file instead of hardcoded value

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -1,7 +1,6 @@
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
-// require("dotenv").config();
-// const JWT_SECRET = process.env.JWT_SECRET;
-const JWT_SECRET = "Shivamwilllove@69$";
+const JWT_SECRET = process.env.JWT_KEY;
 
 const fetchuser = (req, res, next) => {
   // Get the user from the jwt token and add id to req object
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
@@ -5,9 +6,7 @@ const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
 var fetchuser = require("../middleware/fetchuser");
-// require("dotenv").config();
-// const JWT_SECRET = process.env.JWT_SECRET
-const JWT_SECRET = "Shivamwilllove@69$";
+const JWT_SECRET = process.env.JWT_KEY;
 
 // Route 1: Create a user using: POST "/api/auth/createuser". Doesn't require Auth
 router.post(
